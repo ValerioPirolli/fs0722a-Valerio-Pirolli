3c086868077eb2d70d5af81866c1bb8415d0961f
ValerioPirolli <pirollivalerio@gmail.com>
2022-10-31 12:36:53 +0100

    e1

---
+++ m1/s2/e1/java/index.js
@@ -0,0 +1,2 @@
+alert('Usa il pop up per dare dei messaggi');
+window.alert('Sto studiando JS');
---
+++ m1/s2/e1/java/naming.js
@@ -0,0 +1,17 @@
+//var 1 pet_preferito = 'gatto';
+//console.log(1 pet_preferito);
+
+/*var pet = 'gatto';
+var Pet = 'cane';*/
+
+
+var petPreferito = 'criceto';
+var PetPreferito = 'coniglio';
+
+var $pet = 'giraffa';
+console.log($pet);
+var _pet = 'leone';
+console.log(_pet);
+var _pet2 = 'pantera';
+document.write(_pet2);
+//var 12 = 'numero';
